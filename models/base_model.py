eab8fbc8ebb462d8050c06328604e412a7b9591a
Cheikyadam <urbaingande@gmail.com>
2024-01-14 11:02:33 -0800

    4

--- models/base_model.py
+++ models/base_model.py
@@ -15,17 +15,17 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if kwargs is not None or kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key == "id":
-                        self.id = value
-                    elif key == "created_at":
-                        self.created_at = datetime.fromisoformat(value)
+        #if kwargs is not None or kwargs:
+         #   for key, value in kwargs.items():
+          #      if key != "__class__":
+           #         if key == "id":
+            #            self.id = value
+              #      elif key == "created_at":
+             """           self.created_at = datetime.fromisoformat(value)
                     elif key == "updated_at":
                         self.updated_at = datetime.fromisoformat(value)
                     else:
-                        self.key = value
+                        self.key = value"""
         storage.new(self)
 
     def __str__(self):
